8d40f3d1921bd3e6f9d456c17ebf27c40c054dbe
alu0100819182 <alu0100819182@ull.edu.es>
2016-10-21 11:01:17 +0100

    Subiendo proyecto

---
+++ Canarias/Assets/ColoryGiro.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColoryGiro : MonoBehaviour {
+	public delegate void metodoDelegado();
+	public static metodoDelegado Evento;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		GetComponent<Renderer>().material.color = Color.blue;
+		Evento ();
+	}
+}
---
+++ Canarias/Assets/Giro.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Giro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		transform.Rotate(0, 90, 0);
+	}
+}
